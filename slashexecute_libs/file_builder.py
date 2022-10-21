569816ea4e9dc8f41f7812db4187175d13628cb8
HeDeAn <105547153+HeDeAnTheonlyone@users.noreply.github.com>
2022-10-21 12:23:19 +0200

    Update file_builder.py

--- slashexecute_libs/file_builder.py
+++ slashexecute_libs/file_builder.py
@@ -1,12 +1,8 @@
 
-class PackMcMeta:
-    def __init__(self, packFormat):
-        self.packMcMeta = "{\n\t\"pack\": {\n\t\t\"pack_format\":" + packFormat + ",\n\t\t\"description\":\"Datapark datapack\"\n\t}\n}"
-    def __str__(self):
-        return "{seft.packMcMeta}"
+def PackMcMeta(packFormat) :
+    packMcMeta = "{\n\t\"pack\": {\n\t\t\"pack_format\":" + packFormat + ",\n\t\t\"description\":\"Datapark datapack\"\n\t}\n}"
+    return packMcMeta
 
-class FunTagJson:
-    def __init__(self, packName, funType):
-        self.funTag = "{\n\t\"value\": [\n\t\t\"" + packName + ":" + funType + "\"\n\t]\n}"
-    def __str__(self):
-        return "{seft.funTag}"
+def FunTagJson(packName, funType) :
+    self.funTag = "{\n\t\"value\": [\n\t\t\"" + packName + ":" + funType + "\"\n\t]\n}"
+    return funTag
